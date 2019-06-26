bde9cd20c387a0e3633578c9b4ff77ffecc6712a
Bahar <bshidrang@yahoo.com>
2019-06-26 17:59:04 +0200

    email work

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
 
 from datetime import datetime
-from app import db, login
+from app import db, login, app
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
+from time import time
+import jwt
 
 followers = db.Table('followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
